e0bd77a4d5ae955450851db219ca0a7eb08ad438
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-09 13:45:59 -0300

    Update link.py

---
+++ Models/link.py
@@ -0,0 +1,7 @@
+class Link:
+    def __init__(self, id, usuario_id, titulo, url, ordem):
+        self.id = id
+        self.usuario_id = usuario_id
+        self.titulo = titulo
+        self.url = url
+        self.ordem = ordem
