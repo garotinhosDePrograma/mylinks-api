12f0126c7de664ad4fb54a85fc341c0be726ece7
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-09 15:16:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+import os
 from Controllers.userController import user_bp
 from Controllers.linkController import link_bp
 
@@ -8,3 +9,7 @@ CORS(app)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(link_bp)
+
+@app.route("/uploads/<path:filename>")
+def uploaded_file():
+    return send_from_directory(os.path.join(app.root_path, "uploads"), filename)
