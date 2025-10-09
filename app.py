0aa86fda15e45666c73ce8160578af535d0172be
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-09 14:27:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from Controllers.userController import user_bp
+from Controllers.linkController import link_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(user_bp)
+app.register_blueprint(link_bp)
