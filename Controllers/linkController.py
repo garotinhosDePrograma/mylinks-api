32730d2e227a062e4c7840f5860229698fae7af1
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-14 21:59:03 -0300

    Update linkController.py

--- Controllers/linkController.py
+++ Controllers/linkController.py
@@ -8,7 +8,7 @@ worker = LinkWorker()
 @link_bp.route("/links", methods=["GET"])
 @token_required
 def get_links(usuario_id):
-    return jsonify(worker.listar(usuario_id))
+    return jsonify(worker.getAll(usuario_id))
 
 @link_bp.route("/links", methods=["POST"])
 @token_required
@@ -18,7 +18,7 @@ def create_link(usuario_id):
     url = data.get("url")
     if not all([titulo, url]):
         return jsonify({"error": "Campos Obrigatórios"}), 400
-    return jsonify(worker.criar(usuario_id, titulo, url))
+    return jsonify(worker.create(usuario_id, titulo, url))
 
 @link_bp.route("/links/<int:id>", methods=["PUT"])
 @token_required
@@ -26,15 +26,15 @@ def update_link(usuario_id, id):
     data = request.get_json()
     titulo = data.get("titulo")
     url = data.get("url")
-    return jsonify(worker.editar(usuario_id, id, titulo, url))
+    return jsonify(worker.update(usuario_id, id, titulo, url))
 
 @link_bp.route("/links/<int:id>", methods=["DELETE"])
 @token_required
 def delete_link(usuario_id, id):
-    return jsonify(worker.excluir(usuario_id, id))
+    return jsonify(worker.delete(usuario_id, id))
 
 @link_bp.route("/links/reorder", methods=["PUT"])
 @token_required
 def reorder_links(usuario_id):
     data = request.get_json()
-    return jsonify(worker.reordenar(usuario_id, data))
+    return jsonify(worker.reorder(usuario_id, data))
