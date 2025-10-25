d017759e98262dfce5ee35ee296ed7f87b980799
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-25 16:15:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,3 @@ CORS(app)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(link_bp)
-
-@app.route("/uploads/<path:filename>")
-def uploaded_file(filename):
-    return send_from_directory(os.path.join(app.root_path, "uploads"), filename)
