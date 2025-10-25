054618519d4cf2761346cf61bc298b94aa42136d
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-25 16:37:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-import os
 from Controllers.userController import user_bp
 from Controllers.linkController import link_bp
+import logging
 
 app = Flask(__name__)
 CORS(app)
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(link_bp)
