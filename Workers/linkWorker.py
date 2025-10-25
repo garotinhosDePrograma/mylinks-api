75bcf1cb2e6313993938f9d48c59fc760586a6c2
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-25 16:03:46 -0300

    Update linkWorker.py

--- Workers/linkWorker.py
+++ Workers/linkWorker.py
@@ -9,21 +9,25 @@ class LinkWorker:
     def create(self, usuario_id, titulo, url):
         links = repo.getAll(usuario_id)
         nova_ordem = len(links) + 1
-        repo.create(usuario_id, titulo, url, nova_ordem)
+        sucesso = repo.create(usuario_id, titulo, url, nova_ordem)
+        if not sucesso:
+            return {"error": "Erro ai adicionar link"}, 500
         return {"message": "Link adicionado com sucesso"}
     
     def update(self, titulo, url, id, usuario_id):
-        repo.update(titulo, url, id, usuario_id)
+        sucesso = repo.update(titulo, url, id, usuario_id)
+        if not sucesso:
+            return {"error": "Erro ao adicionar link"}, 500
         return {"message": "Link atualizado com sucesso"}
     
     def delete(self, usuario_id, id):
-        repo.delete(id, usuario_id)
+        sucesso = repo.delete(id, usuario_id)
+        if not sucesso:
+            return {"error": "Erro ao remover link"}, 500
         return {"message": "Link removido com sucesso"}
 
     def reorder(self, usuario_id, links):
-        repo.reorder(usuario_id, links)
+        sucesso = repo.reorder(usuario_id, links)
+        if not sucesso:
+            return {"error": "Erro ao tentar reordenar links"}, 500
         return {"message": "Links reordenados com sucesso"}
-
-
-
-
