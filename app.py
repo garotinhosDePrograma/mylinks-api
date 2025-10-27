d0ba67b4c80e898c1c6714d38f3efbe2856a9d06
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-10-27 13:41:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask_cors import CORS
 from Controllers.userController import user_bp
 from Controllers.linkController import link_bp
 import logging
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+log_path = os.path.join("/tmp", "app.log")
 
 logging.basicConfig(
     level=logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("app.log"),
+        logging.FileHandler(log_path),
         logging.StreamHandler()
     ]
 )
