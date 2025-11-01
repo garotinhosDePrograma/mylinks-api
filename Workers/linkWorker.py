5b4e322bb33b6f22a5d5f6a91d556f5f4819054f
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-11-01 10:24:53 -0300

    Update linkWorker.py

--- Workers/linkWorker.py
+++ Workers/linkWorker.py
@@ -1,4 +1,3 @@
-from flask import jsonify
 from Repositories.linkRepository import LinkRepository
 
 repo = LinkRepository()
@@ -7,36 +6,38 @@ class LinkWorker:
     def getAll(self, usuario_id):
         links = repo.getAll(usuario_id)
         if links is None:
-            return jsonify({"error": "Erro ao buscar links"}), 500
+            return {"error": "Erro ao buscar links"}, 500
         return links
     
     def create(self, usuario_id, titulo, url):
         links = repo.getAll(usuario_id)
         if links is None:
-            return jsonify({"error": "Erro ao buscar links existentes"}), 500
+            return {"error": "Erro ao buscar links existentes"}, 500
         
         nova_ordem = len(links) + 1
         sucesso = repo.create(usuario_id, titulo, url, nova_ordem)
         
         if not sucesso:
-            return jsonify({"error": "Erro ao adicionar link"}), 500
-        return jsonify({"message": "Link adicionado com sucesso"}), 200
+            return {"error": "Erro ao adicionar link"}, 500
+        return {"message": "Link adicionado com sucesso"}
     
     def update(self, titulo, url, id, usuario_id):
         sucesso = repo.update(titulo, url, id, usuario_id)
+        
         if not sucesso:
-            return jsonify({"error": "Erro ao adicionar link"}), 500
-        return jsonify({"message": "Link atualizado com sucesso"}), 200
+            return {"error": "Erro ao atualizar link"}, 500
+        return {"message": "Link atualizado com sucesso"}
     
     def delete(self, usuario_id, id):
         sucesso = repo.delete(id, usuario_id)
+        
         if not sucesso:
-            return jsonify({"error": "Erro ao remover link"}), 500
-        return jsonify({"message": "Link removido com sucesso"}), 200
+            return {"error": "Erro ao remover link"}, 500
+        return {"message": "Link removido com sucesso"}
 
     def reorder(self, usuario_id, links):
         sucesso = repo.reorder(usuario_id, links)
+        
         if not sucesso:
-            return jsonify({"error": "Erro ao tentar reordenar links"}), 500
-
-        return jsonify({"message": "Links reordenados com sucesso"}), 200
+            return {"error": "Erro ao reordenar links"}, 500
+        return {"message": "Links reordenados com sucesso"}
