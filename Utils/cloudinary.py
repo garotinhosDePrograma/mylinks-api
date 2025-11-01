350dacd6f221ee04b19cb2c774db106b10f581c1
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-11-01 10:30:28 -0300

    Update cloudinary.py

--- Utils/cloudinary.py
+++ Utils/cloudinary.py
@@ -1,3 +1,9 @@
+import cloudinary
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def configure_cloudinary():
     cloudinary.config(
         cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
