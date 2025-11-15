bb07671c078ecaf083e563eceab92c4b28ef7fe9
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-11-15 11:48:07 -0300

    Update user.py

--- Models/user.py
+++ Models/user.py
@@ -5,3 +5,29 @@ class User:
         self.email = email
         self.senha = senha
         self.foto_perfil = foto_perfil
+    
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "foto_perfil": self.foto_perfil
+        }
+        
+        if include_senha:
+            data["senha"] = self.senha
+        
+        return data
+    
+    def to_public_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "foto_perfil": self.foto_perfil
+        }
+    
+    def __repr__(self):
+        return f"<User id={self.id} username='{self.username}' email='{self.email}'>"
+
+    def __str__(self):
+        return self.username
