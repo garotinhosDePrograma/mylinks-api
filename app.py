6a54c0b9852dd0eaf1612a724d9d5d30c13ba2a4
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-12-14 18:09:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from flask_cors import CORS
 from Controllers.userController import user_bp
 from Controllers.linkController import link_bp
 import logging
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://mylinks-352x.onrender.com",
+            "http://localhost:8080"
+        ],
+        "methods": ["GET", "POST" "PUT" "DELETE"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 
 logging.basicConfig(
     level=logging.ERROR,
