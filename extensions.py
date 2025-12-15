ddfb03ab279403b243e721c53cfe2e503c65f00a
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-12-15 14:43:25 -0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -5,5 +5,5 @@ limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"],
     storage_uri="memory://",
-    strategy="fixed-window-elastic-expiry"
+    strategy="fixed-window"
 )
