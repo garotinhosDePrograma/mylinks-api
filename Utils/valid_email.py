0bec3259a316741035e48da7b4ed0e24ba69a7ce
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-12-17 18:03:40 +0000

    valid_email

--- Utils/valid_email.py
+++ Utils/valid_email.py
@@ -1,1 +1,15 @@
+import re
 
+def is_valid_email(email):
+    if not email or not isinstance(email, str):
+        return False
+    
+    pattern = r'^[a-zA-z0-9._%+-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,}$'
+    
+    if len(email) > 254:
+        return False
+    
+    if '..' in email:
+        return False
+    
+    return bool(re.match(pattern, email))
