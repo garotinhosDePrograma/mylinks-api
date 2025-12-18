73c011abf16fbe5a7e76c86edfc1038687354599
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-12-18 17:38:18 -0300

    Update valid_email.py

--- Utils/valid_email.py
+++ Utils/valid_email.py
@@ -4,7 +4,7 @@ def is_valid_email(email):
     if not email or not isinstance(email, str):
         return False
     
-    pattern = r'^[a-zA-z0-9._%+-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,}$'
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     
     if len(email) > 254:
         return False
