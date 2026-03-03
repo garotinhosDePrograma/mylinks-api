56c1d9bb1098cc6f5a23c16cd9d854bdf9b2eaff
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-03-03 08:28:54 -0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,7 +3,7 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["500 per day", "100 per hour"],
     storage_uri="memory://",
     strategy="fixed-window"
 )
